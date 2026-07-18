87317c34c6a27e457854d07a8f2c18596bbe7fe8
jawabexpress <jawabexpress@gmail.com>
2026-07-19 07:23:06 +0800

    Update kategori1.js

--- data/darjah1/matematik/kategori1.js
+++ data/darjah1/matematik/kategori1.js
@@ -225,4 +225,681 @@ D:"13",
 answer:"B"
 }
 
+{
+{
+question:"Apakah nombor bagi perkataan sebelas?",
+A:"10",
+B:"11",
+C:"12",
+D:"13",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan tiga belas?",
+A:"12",
+B:"13",
+C:"14",
+D:"15",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan empat belas?",
+A:"13",
+B:"14",
+C:"15",
+D:"16",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan enam belas?",
+A:"15",
+B:"16",
+C:"17",
+D:"18",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan tujuh belas?",
+A:"16",
+B:"17",
+C:"18",
+D:"19",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan sembilan belas?",
+A:"17",
+B:"18",
+C:"19",
+D:"20",
+answer:"C"
+},
+
+{
+question:"Nombor manakah lebih besar?",
+A:"6",
+B:"8",
+C:"3",
+D:"5",
+answer:"B"
+},
+
+{
+question:"Nombor manakah lebih kecil?",
+A:"9",
+B:"7",
+C:"4",
+D:"8",
+answer:"C"
+},
+
+{
+question:"Susunan menaik yang betul?",
+A:"5,4,3",
+B:"3,4,5",
+C:"5,3,4",
+D:"4,5,3",
+answer:"B"
+},
+
+{
+question:"Susunan menurun yang betul?",
+A:"9,8,7",
+B:"7,8,9",
+C:"8,7,9",
+D:"7,9,8",
+answer:"A"
+},
+
+{
+question:"Apakah nombor yang hilang? 1,2,__,4",
+A:"3",
+B:"5",
+C:"6",
+D:"7",
+answer:"A"
+},
+
+{
+question:"Apakah nombor yang hilang? 5,6,__,8",
+A:"4",
+B:"7",
+C:"9",
+D:"10",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 10,__,12",
+A:"9",
+B:"11",
+C:"13",
+D:"14",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 15,16,__,18",
+A:"14",
+B:"17",
+C:"19",
+D:"20",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 18,19,__,21",
+A:"17",
+B:"18",
+C:"20",
+D:"22",
+answer:"C"
+},
+
+{
+question:"Berapakah nilai nombor ini: 6?",
+A:"Enam",
+B:"Tujuh",
+C:"Lapan",
+D:"Sembilan",
+answer:"A"
+},
+
+{
+question:"Berapakah nilai nombor ini: 14?",
+A:"Dua belas",
+B:"Tiga belas",
+C:"Empat belas",
+D:"Lima belas",
+answer:"C"
+},
+
+{
+question:"Berapakah nilai nombor ini: 17?",
+A:"Lima belas",
+B:"Enam belas",
+C:"Tujuh belas",
+D:"Lapan belas",
+answer:"C"
+},
+
+{
+question:"Berapakah nilai nombor ini: 20?",
+A:"Sepuluh",
+B:"Lima belas",
+C:"Dua puluh",
+D:"Tiga puluh",
+answer:"C"
+},
+
+{
+question:"Nombor selepas 7 ialah?",
+A:"6",
+B:"7",
+C:"8",
+D:"9",
+answer:"C"
+},
+
+{
+question:"Nombor sebelum 10 ialah?",
+A:"8",
+B:"9",
+C:"10",
+D:"11",
+answer:"B"
+},
+
+{
+question:"Nombor selepas 14 ialah?",
+A:"13",
+B:"14",
+C:"15",
+D:"16",
+answer:"C"
+},
+
+{
+question:"Nombor sebelum 18 ialah?",
+A:"16",
+B:"17",
+C:"18",
+D:"19",
+answer:"B"
+},
+
+{
+question:"Nombor antara 11 dan 13 ialah?",
+A:"10",
+B:"11",
+C:"12",
+D:"13",
+answer:"C"
+},
+
+{
+question:"Nombor antara 16 dan 18 ialah?",
+A:"15",
+B:"16",
+C:"17",
+D:"18",
+answer:"C"
+}
+
+{
+{
+question:"Apakah nombor yang lebih kecil antara 12 dan 15?",
+A:"12",
+B:"15",
+C:"17",
+D:"20",
+answer:"A"
+},
+
+{
+question:"Apakah nombor yang lebih besar antara 8 dan 13?",
+A:"5",
+B:"8",
+C:"13",
+D:"7",
+answer:"C"
+},
+
+{
+question:"Pilih nombor ganjil.",
+A:"2",
+B:"4",
+C:"5",
+D:"6",
+answer:"C"
+},
+
+{
+question:"Pilih nombor genap.",
+A:"3",
+B:"5",
+C:"7",
+D:"8",
+answer:"D"
+},
+
+{
+question:"Berapakah nombor yang ditunjukkan oleh perkataan tiga?",
+A:"1",
+B:"2",
+C:"3",
+D:"4",
+answer:"C"
+},
+
+{
+question:"Berapakah nombor yang ditunjukkan oleh perkataan enam?",
+A:"4",
+B:"5",
+C:"6",
+D:"7",
+answer:"C"
+},
+
+{
+question:"Berapakah nombor yang ditunjukkan oleh perkataan dua belas?",
+A:"10",
+B:"11",
+C:"12",
+D:"13",
+answer:"C"
+},
+
+{
+question:"Berapakah nombor yang ditunjukkan oleh perkataan lima belas?",
+A:"13",
+B:"14",
+C:"15",
+D:"16",
+answer:"C"
+},
+
+{
+question:"Berapakah nombor yang ditunjukkan oleh perkataan lapan belas?",
+A:"16",
+B:"17",
+C:"18",
+D:"19",
+answer:"C"
+},
+
+{
+question:"Berapakah nombor yang ditunjukkan oleh perkataan dua puluh?",
+A:"18",
+B:"19",
+C:"20",
+D:"21",
+answer:"C"
+},
+
+{
+question:"Susunan nombor yang betul dari kecil ke besar?",
+A:"2,4,6",
+B:"6,4,2",
+C:"4,2,6",
+D:"6,2,4",
+answer:"A"
+},
+
+{
+question:"Susunan nombor yang betul dari besar ke kecil?",
+A:"3,5,7",
+B:"7,5,3",
+C:"5,7,3",
+D:"3,7,5",
+answer:"B"
+},
+
+{
+question:"Nombor selepas 16 ialah?",
+A:"15",
+B:"16",
+C:"17",
+D:"18",
+answer:"C"
+},
+
+{
+question:"Nombor sebelum 16 ialah?",
+A:"14",
+B:"15",
+C:"16",
+D:"17",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 2,3,__,5",
+A:"1",
+B:"4",
+C:"6",
+D:"7",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 7,8,__,10",
+A:"6",
+B:"8",
+C:"9",
+D:"11",
+answer:"C"
+},
+
+{
+question:"Apakah nombor yang hilang? 12,__,14",
+A:"11",
+B:"13",
+C:"15",
+D:"16",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 17,__,19",
+A:"16",
+B:"18",
+C:"20",
+D:"21",
+answer:"B"
+},
+
+{
+question:"Nombor 9 dibaca sebagai?",
+A:"Lapan",
+B:"Sembilan",
+C:"Sepuluh",
+D:"Sebelas",
+answer:"B"
+},
+
+{
+question:"Nombor 13 dibaca sebagai?",
+A:"Dua belas",
+B:"Tiga belas",
+C:"Empat belas",
+D:"Lima belas",
+answer:"B"
+},
+
+{
+question:"Nombor 16 dibaca sebagai?",
+A:"Lima belas",
+B:"Enam belas",
+C:"Tujuh belas",
+D:"Lapan belas",
+answer:"B"
+},
+
+{
+question:"Nombor 19 dibaca sebagai?",
+A:"Lapan belas",
+B:"Sembilan belas",
+C:"Dua puluh",
+D:"Tujuh belas",
+answer:"B"
+},
+
+{
+question:"Manakah nombor terkecil?",
+A:"11",
+B:"7",
+C:"15",
+D:"20",
+answer:"B"
+},
+
+{
+question:"Manakah nombor terbesar?",
+A:"12",
+B:"18",
+C:"9",
+D:"14",
+answer:"B"
+},
+
+{
+question:"Apakah nombor di tengah antara 4 dan 6?",
+A:"3",
+B:"4",
+C:"5",
+D:"7",
+answer:"C"
+}
+
+{
+{
+question:"Apakah nombor selepas 11?",
+A:"10",
+B:"11",
+C:"12",
+D:"13",
+answer:"C"
+},
+
+{
+question:"Apakah nombor sebelum 14?",
+A:"12",
+B:"13",
+C:"14",
+D:"15",
+answer:"B"
+},
+
+{
+question:"Pilih nombor yang betul untuk perkataan empat belas.",
+A:"12",
+B:"13",
+C:"14",
+D:"15",
+answer:"C"
+},
+
+{
+question:"Pilih nombor yang betul untuk perkataan tujuh belas.",
+A:"15",
+B:"16",
+C:"17",
+D:"18",
+answer:"C"
+},
+
+{
+question:"Pilih nombor yang betul untuk perkataan sembilan belas.",
+A:"17",
+B:"18",
+C:"19",
+D:"20",
+answer:"C"
+},
+
+{
+question:"Apakah nombor selepas 18?",
+A:"17",
+B:"18",
+C:"19",
+D:"20",
+answer:"C"
+},
+
+{
+question:"Apakah nombor sebelum 20?",
+A:"18",
+B:"19",
+C:"20",
+D:"21",
+answer:"B"
+},
+
+{
+question:"Susun nombor berikut: 8,6,7",
+A:"6,7,8",
+B:"8,7,6",
+C:"7,6,8",
+D:"8,6,7",
+answer:"A"
+},
+
+{
+question:"Susun nombor berikut: 15,13,14",
+A:"15,14,13",
+B:"13,14,15",
+C:"14,15,13",
+D:"13,15,14",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 9,10,__,12",
+A:"8",
+B:"10",
+C:"11",
+D:"13",
+answer:"C"
+},
+
+{
+question:"Apakah nombor yang hilang? 13,14,__,16",
+A:"12",
+B:"15",
+C:"17",
+D:"18",
+answer:"B"
+},
+
+{
+question:"Apakah nombor yang hilang? 16,17,__,19",
+A:"15",
+B:"18",
+C:"20",
+D:"21",
+answer:"B"
+},
+
+{
+question:"Nombor 5 berada selepas nombor?",
+A:"3",
+B:"4",
+C:"6",
+D:"7",
+answer:"B"
+},
+
+{
+question:"Nombor 10 berada sebelum nombor?",
+A:"8",
+B:"9",
+C:"11",
+D:"12",
+answer:"C"
+},
+
+{
+question:"Yang manakah nombor genap?",
+A:"5",
+B:"7",
+C:"9",
+D:"10",
+answer:"D"
+},
+
+{
+question:"Yang manakah nombor ganjil?",
+A:"2",
+B:"4",
+C:"6",
+D:"7",
+answer:"D"
+},
+
+{
+question:"Berapa bilangan nombor dari 1 hingga 5?",
+A:"3",
+B:"4",
+C:"5",
+D:"6",
+answer:"C"
+},
+
+{
+question:"Berapa bilangan nombor dari 10 hingga 15?",
+A:"4",
+B:"5",
+C:"6",
+D:"7",
+answer:"C"
+},
+
+{
+question:"Apakah nombor paling kecil?",
+A:"16",
+B:"12",
+C:"19",
+D:"14",
+answer:"B"
+},
+
+{
+question:"Apakah nombor paling besar?",
+A:"13",
+B:"17",
+C:"11",
+D:"15",
+answer:"B"
+},
+
+{
+question:"Nombor 20 datang selepas nombor?",
+A:"17",
+B:"18",
+C:"19",
+D:"21",
+answer:"C"
+},
+
+{
+question:"Nombor 1 datang sebelum nombor?",
+A:"0",
+B:"2",
+C:"3",
+D:"4",
+answer:"B"
+},
+
+{
+question:"Apakah nilai nombor ini: 11?",
+A:"Sepuluh",
+B:"Sebelas",
+C:"Dua belas",
+D:"Tiga belas",
+answer:"B"
+},
+
+{
+question:"Apakah nilai nombor ini: 15?",
+A:"Empat belas",
+B:"Lima belas",
+C:"Enam belas",
+D:"Tujuh belas",
+answer:"B"
+},
+
+{
+question:"Apakah nilai nombor ini: 18?",
+A:"Enam belas",
+B:"Tujuh belas",
+C:"Lapan belas",
+D:"Sembilan belas",
+answer:"C"
+}
 ];
