5e5cb5cd76b74d892bd16bb75a061aa7b136505c
jawabexpress <jawabexpress@gmail.com>
2026-07-19 07:13:53 +0800

    Create kategori1.js

---
+++ data/darjah1/matematik/kategori1.js
@@ -0,0 +1,228 @@
+export const kategori1 = [
+
+{
+question:"Apakah nombor bagi perkataan satu?",
+A:"1",
+B:"2",
+C:"3",
+D:"4",
+answer:"A"
+},
+
+{
+question:"Apakah nombor bagi perkataan dua?",
+A:"1",
+B:"2",
+C:"3",
+D:"5",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan tiga?",
+A:"2",
+B:"3",
+C:"4",
+D:"6",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan empat?",
+A:"3",
+B:"4",
+C:"5",
+D:"7",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan lima?",
+A:"4",
+B:"5",
+C:"6",
+D:"8",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan enam?",
+A:"5",
+B:"6",
+C:"7",
+D:"9",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan tujuh?",
+A:"6",
+B:"7",
+C:"8",
+D:"10",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan lapan?",
+A:"7",
+B:"8",
+C:"9",
+D:"11",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan sembilan?",
+A:"8",
+B:"9",
+C:"10",
+D:"12",
+answer:"B"
+},
+
+{
+question:"Apakah nombor bagi perkataan sepuluh?",
+A:"9",
+B:"10",
+C:"11",
+D:"12",
+answer:"B"
+},
+
+{
+question:"Apakah nombor selepas 10?",
+A:"9",
+B:"10",
+C:"11",
+D:"12",
+answer:"C"
+},
+
+{
+question:"Apakah nombor sebelum 5?",
+A:"3",
+B:"4",
+C:"6",
+D:"7",
+answer:"B"
+},
+
+{
+question:"Apakah nombor selepas 15?",
+A:"14",
+B:"15",
+C:"16",
+D:"17",
+answer:"C"
+},
+
+{
+question:"Apakah nombor sebelum 20?",
+A:"18",
+B:"19",
+C:"20",
+D:"21",
+answer:"B"
+},
+
+{
+question:"Nombor manakah menunjukkan tujuh?",
+A:"5",
+B:"6",
+C:"7",
+D:"8",
+answer:"C"
+},
+
+{
+question:"Nombor manakah menunjukkan sembilan?",
+A:"7",
+B:"8",
+C:"9",
+D:"10",
+answer:"C"
+},
+
+{
+question:"Nombor manakah menunjukkan dua belas?",
+A:"10",
+B:"11",
+C:"12",
+D:"13",
+answer:"C"
+},
+
+{
+question:"Nombor manakah menunjukkan lima belas?",
+A:"13",
+B:"14",
+C:"15",
+D:"16",
+answer:"C"
+},
+
+{
+question:"Nombor manakah menunjukkan lapan belas?",
+A:"16",
+B:"17",
+C:"18",
+D:"19",
+answer:"C"
+},
+
+{
+question:"Nombor manakah menunjukkan dua puluh?",
+A:"18",
+B:"19",
+C:"20",
+D:"21",
+answer:"C"
+},
+
+{
+question:"Susunan nombor yang betul ialah?",
+A:"1,2,3",
+B:"3,2,1",
+C:"2,1,3",
+D:"3,1,2",
+answer:"A"
+},
+
+{
+question:"Nombor paling kecil ialah?",
+A:"5",
+B:"2",
+C:"8",
+D:"10",
+answer:"B"
+},
+
+{
+question:"Nombor paling besar ialah?",
+A:"4",
+B:"7",
+C:"9",
+D:"3",
+answer:"C"
+},
+
+{
+question:"Apakah nombor selepas 19?",
+A:"18",
+B:"19",
+C:"20",
+D:"21",
+answer:"C"
+},
+
+{
+question:"Apakah nombor sebelum 12?",
+A:"10",
+B:"11",
+C:"12",
+D:"13",
+answer:"B"
+}
+
+];
