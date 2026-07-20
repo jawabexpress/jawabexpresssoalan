3d0127280c16e42e0f53ad60d02a16680d5c956d
jawabexpress <jawabexpress@gmail.com>
2026-07-20 23:56:09 +0800

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,42 @@
+const questionBank = {
+
+darjah1:{
+    
+matematik:{
+
+kenalNombor:[
+
+{
+soalan:"Apakah nombor bagi perkataan lima?",
+A:"3",
+B:"5",
+C:"7",
+D:"9",
+jawapan:"B"
+},
+
+{
+soalan:"Apakah nombor selepas 8?",
+A:"7",
+B:"8",
+C:"9",
+D:"10",
+jawapan:"C"
+},
+
+{
+soalan:"Nombor manakah lebih besar?",
+A:"2",
+B:"4",
+C:"6",
+D:"1",
+jawapan:"C"
+}
+
+]
+
+}
+
+}
+
+};
